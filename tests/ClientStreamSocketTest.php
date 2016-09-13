4daaa1856f4d3a0126a2cd76c3dd3f3ef6ffd96e
Chris Langton <chrislangton84@gmail.com>
2016-09-13 22:21:10 +1000

    added several ClientStreamSocket tests

--- tests/ClientStreamSocketTest.php
+++ tests/ClientStreamSocketTest.php
@@ -2,14 +2,44 @@
 use PHPUnit\Framework\TestCase;
 use sockets\ClientStreamSocket;
 
-class ClientStreamSocketTest extends TestCase {
+class ClientStreamSocketTest extends TestCase
+{
     protected $clientStreamSocket;
-    
-    public function __construct() {
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
         $this->clientStreamSocket = new ClientStreamSocket();
     }
 
-    public function testInstace() {
+    public function testInstance()
+    {
         $this->assertInstanceOf('sockets\\ClientStreamSocket', $this->clientStreamSocket);
+    }
+
+    public function testJobId()
+    {
+        $this->assertInternalType('string', $this->clientStreamSocket->getJobId());
+    }
+
+    public function testData()
+    {
+        $this->assertNotNull($this->clientStreamSocket->getData());
+        $this->assertNotFalse($this->clientStreamSocket->getData());
+    }
+
+    public function testHeaders()
+    {
+        $this->assertInternalType('string', $this->clientStreamSocket->getHeaders());
+    }
+
+    public function testLastDataLength()
+    {
+        $this->assertInternalType('int', $this->clientStreamSocket->getLastDataLength());
+    }
+
+    public function testStatus()
+    {
+        $this->assertInternalType('array', $this->clientStreamSocket->getStatus());
     }
 }
